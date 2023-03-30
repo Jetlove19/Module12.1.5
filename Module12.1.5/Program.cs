6b4bede554db780129b3664261568f4897fdf4d0
Jetlove19 <rbrorlove@gmail.com>
2023-03-30 03:50:14 +0300

    Добавьте файлы проекта.

---
+++ Module12.1.5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Module12._1._5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                List<Users> listUsers = new()
+        {
+           new Users() { Name = "Павел", Login = Guid.NewGuid().ToString().Substring(1, 7), IsPremium = false },
+           new Users() { Name = "Ольга", Login = Guid.NewGuid().ToString().Substring(1, 7), IsPremium = true },
+           new Users() { Name = "Валентина", Login = Guid.NewGuid().ToString().Substring(1, 7), IsPremium = false },
+        };
+                foreach (var Premium in listUsers)
+                {
+                    if (!Premium.IsPremium)
+                    {
+                        Console.Write(Premium.Name + " ");
+                        ADS.ShowAds();
+                    }
+                }
+            }
+        }
+    }
+}
